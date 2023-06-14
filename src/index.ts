da3d02a64384b28e5df532be1d80b5cdc1deca8b
Muhammad Kaif <xcoder304@gmail.com>
2023-06-14 11:38:13 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const server = http.createServer((req: any, res: any) => {
   res.end("Hello, world!");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
